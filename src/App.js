661620103672cab3eb427dc8c37b85543532072b
KAYLE KINLEY STAANA <98571657+Dracorecord@users.noreply.github.com>
2022-03-29 12:20:01 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "./Components/Main";
+import './Components/style.css';
+
+function App() {
+  return (
+    <>
+    <center><h1>Pokemon Codex</h1></center>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
